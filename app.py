0c3a2cf8726f7efb96f33ddf65a53d0ba16bb3d9
UditTiwari <udittiwari1502@gmail.com>
2023-12-18 10:32:23 +0530

    use of csv.DictReader(f)

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def home():
 
 def read_csv(csv_file):
     with open(csv_file,'r') as f:
+        # csv.DictReader(f): This is a function provided by the csv module in Python. It takes a file object f (which should be an opened CSV file) and returns a reader object that iterates over lines in the given CSV file. Each row is returned as an ordered dictionary where the keys are the fieldnames and the values are the row entries.
         csv_reader = csv.DictReader(f)
         data = [row for row in csv_reader]
     return data
